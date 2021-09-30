4d20e84fa239d41575ae624e8c77c450ba17687a
nrodrigues64 <34452117+nrodrigues64@users.noreply.github.com>
2021-09-30 14:19:03 +0200

    Create test_motors.py
    Create test_motors.py

---
+++ tests/test_motors.py
@@ -0,0 +1,14 @@
+from motors import Motors
+import pypot.dynamixel as dyn
+import time
+
+ports = dyn.get_available_ports()
+if not ports:
+	exit('No port')
+
+dxl_io = Motors(ports[0])
+dxl_io.go_forward()
+time.sleep(2)
+dxl_io.turn_left()
+time.sleep(2)
+dxl_io.stop()
