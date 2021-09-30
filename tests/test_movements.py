84bd68de7848d782c358829e73d658a4a6668eb8
nrodrigues64 <34452117+nrodrigues64@users.noreply.github.com>
2021-09-30 14:40:31 +0200

    Update test_movements.py

--- tests/test_movements.py
+++ tests/test_movements.py
@@ -1,1 +1,10 @@
+from Motors import Motors
+import pypot.dynamixel as dyn
+import time
 
+ports = dyn.get_available_ports()
+if not ports:
+	exit('No port')
+
+dxl_io = Motors(ports[0])
+dxl_io.rotate_angle_clockwise(90)
