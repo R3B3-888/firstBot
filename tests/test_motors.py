67287f1b48c15f74229a289d2b4e809f51c183e1
nrodrigues64 <34452117+nrodrigues64@users.noreply.github.com>
2021-09-30 16:49:08 +0200

    Update test_motors.py

--- tests/test_motors.py
+++ tests/test_motors.py
@@ -13,8 +13,30 @@ if not ports:
 	exit('No port')
 
 dxl_io = Motors(ports[0])
-dxl_io.go_forward()
-time.sleep(2)
-dxl_io.turn_left()
-time.sleep(2)
-dxl_io.stop()
+d1 = [0,0,0,0.5,0.5,-0.5,-0.5,3]
+d = [0,0,0.5,-0.5,3]
+k = 0.4
+def lets_go(list_direction):
+	i = 0
+	while(1):
+		time.sleep(1)
+		if i == len(d):
+			exit()
+		elif d[i] < 0 and d[i] > -1:
+			dxl_io.turn_left(d[i])
+		elif d[i] > 0 and d[i] < 1:
+			dxl_io.turn_right(d[i])
+		elif d[i] == 0 :
+			dxl_io.go_forward()
+		elif d[i] > 1 or d[i] < -1 :
+			dxl_io.stop()
+		
+		i+=1
+
+
+
+lets_go(d)
+
+
+
+
