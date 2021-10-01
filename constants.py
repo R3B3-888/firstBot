a9dba6c117a3207ef1d83c64a0e42e0275f16758
alhoffmann <alexis.hoffmann@etu.u-bordeaux.fr>
2021-10-01 05:57:50 +0100

    Update for order colors path in the constants

--- constants.py
+++ constants.py
@@ -1,11 +1,17 @@
+import numpy as np
+
+
 OPTIONS_AVAILABLE = ['1']
 IDLEFT = 2
 IDRIGHT = 5
-WHEEL_RADIUS = 2.6 # cm
-DIST_LWHEEL_RWHEEL = 16.1 # cm
+WHEEL_RADIUS = 2.6  # cm
+DIST_LWHEEL_RWHEEL = 16.1  # cm
 
-# GO_TO 
+# line following
+
+ORDER_COLORS_PATH = np.array(["blue", "red", "yellow"])
+
+# go to
 X = 1
 Y = 1
 THETA = 180
-
